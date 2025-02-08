aeebbe00114500f1bb8d81f5f257cf1009ef6ead
Daria10 <147264011+Daria1002@users.noreply.github.com>
2025-02-09 00:25:05 +0200

    Create Participant.java

---
+++ Participant.java
@@ -0,0 +1,27 @@
+public class Participant {
+    private String nume;
+    private String email;
+
+    public Participant() {}
+
+    public Participant(String nume, String email) {
+        this.nume = nume;
+        this.email = email;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
